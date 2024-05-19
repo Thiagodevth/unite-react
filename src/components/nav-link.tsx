c293fd3b82a2bcf0f8a08df6e0a3876a4384c4c6
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-05-19 19:18:04 -0300

    commt no nav-link

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -6,7 +6,7 @@ interface NavLinkProps extends ComponentProps<'a'> {
 
 export function NavLink(props: NavLinkProps) {
     return (
-        <a {...props} className='font-medium text-sm'>
+        <a {...props} className="font-medium text-sm text-zinc-300">
             {props.children}
         </a>
     )
